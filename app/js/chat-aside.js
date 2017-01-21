2a4e785615331a2d914dbe5270c40c9a9faa8b8e
Vlad Usovich <vladiusovich@gmail.com>
2017-01-21 19:36:37 +0300

    fix background. fix media. fix chat-aside

--- app/js/chat-aside.js
+++ app/js/chat-aside.js
@@ -12,7 +12,6 @@ function toggle() {
         chat.style.transform = "translateX(0px)";
         if (w > 900) {
             wrapper.style.marginLeft = "300px";
-            chat.style.backgroundColor = "rgb(85, 85, 85)";
         }
         isToggle = true
     }
@@ -23,53 +22,3 @@ function toggle() {
     }
 }
 
-
-
-/*
-function PushSideNav() {
-    var slideNav;
-    var wrapperPage;
-    var toggleButton;
-    var isToggle = false;
-
-    function toggle() {
-        // var slideNav = document.getElementsByClassName("chat--aside")[0];
-        // var wrapperPage = document.getElementsByClassName("wrap-page")[0];
-
-        if (!isToggle) {
-            console.log(chat);
-            slideNav.style.transform = "translateX(0)";
-            wrapperPage.style.marginLeft = "300px";
-            isToggle = true
-        }
-        else {
-            slideNav.style.transform = "translateX(-300px)";
-            wrapperPage.style.marginLeft = "0";
-            isToggle = false;
-        }
-    }
-    that = this;
-
-    return {
-        init: function (toggleButton, slideNav, wrapper) {
-            that.toggleButton  = $(toggleButton);
-            that.slideNav = $(slideNav);
-            that.wrapper = $(wrapper);
-        },
-
-        get: function () {
-            console.log(that.toggleButton);
-            console.log(that.slideNav);
-            console.log(that.wrapper);
-        }
-    }
-};
-
-
-$( document ).ready(function() {
-    var m = PushSideNav();
-    m.init("chat__toggle", "chat--aside", "wrap-page");
-    m.get();
-
-});
-    */
