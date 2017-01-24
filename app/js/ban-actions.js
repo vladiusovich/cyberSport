103c7049e342526c9ae40ee52258993b3b624e68
Vlad Usovich <vladiusovich@gmail.com>
2017-01-24 21:14:07 +0300

    ban menu like module

--- app/js/ban-actions.js
+++ app/js/ban-actions.js
@@ -1,83 +1,125 @@
 
-(function () {
+var banActions = (function () {
+    var user, userName;
+    var menu = $(".ban-actions-menu");
+
+    function getBanMenu() {
+        menu.css("display", "none");
+        user = $(this).next();
+        userName = user.find(".chat__user-name").text();
+        var position = $(this).offset();
+        display_ban_actions(position.top, position.left);
+    }
+
+    function sendData() {
+        var banOption = $("input[type='radio']:checked").val();
+
+        if (banOption == undefined) {
+            return;
+        }
+        console.log(banOption + "  -- " + userName);
+        $.ajax({
+            type: "POST",
+            url: "SomeServlet/_SomeMethod", // Вызываемый метод на сервере
+            contentType: "application/json; charset=utf-8",
+            data: {banOption: banOption, userName: userName},
+            dataType: "json",
+            success: function (result) {
+                console.log('It worked!');
+                //  returning something
+                console.log('result... ' + result);
+            },
+            error: function (result) {
+                console.log('Error :(');
+            }
+        });
+    }
+
+
+    function display_ban_actions(x, y) {
+        var menu = $(".ban-actions-menu");
+        var styles = {
+            display: "block",
+            opacity: 1,
+            top: x - 30,
+            left: y + 30
+        };
+
+        if (menu.css("display") == "none") {
+            menu.css(styles);
+
+        } else {
+            menu.css( {display: "none"} );
+
+        }
+    }
+
+
+
+    return {
+        displayMenu: getBanMenu,
+        sendData: sendData,
+        getMenu: (function () {
+            return menu;
+        })()
+    }
+
 
 })();
 
-var user, userName;
-
-$( ".chat_ban-actions" ).click(function() {
-    // var peer = $(this).attr("data-peer");
-
-    var menu = $(".ban-actions-menu");
-    menu.css("display", "none");
-
-    user = $(this).next();
-    userName = user.find(".chat__user-name").text();
-
-    var position = $(this).offset();
-    display_ban_actions(position.top, position.left);
-
-})
-
-$( ".ban-actions__act" ).click(function() {
 
 
-    var banOption = $("input[type='radio']:checked").val();
-
-    if (banOption == undefined) {
-        return;
-    }
-
-    console.log(banOption + "  -- " + userName);
-
-    $.ajax({
-        type: "POST",
-        url: "SomeServlet/_SomeMethod", // Вызываемый метод на сервере
-        contentType: "application/json; charset=utf-8",
-        data: {banOption: banOption, userName: userName},
-        dataType: "json",
-        success: function (result) {
-            console.log('It worked!');
-            //  returning something
-            console.log('result... ' + result);
-        },
-        error: function (result) {
-            console.log('Error :(');
-        }
-    });
-});
-
-
-function display_ban_actions(x, y) {
-    var menu = $(".ban-actions-menu");
-    var styles = {
-        display: "block",
-        opacity: 1,
-        top: x - 30,
-        left: y + 30
-    };
-
-    if (menu.css("display") == "none") {
-        menu.css(styles);
-
-    } else {
-        menu.css( {display: "none"} );
-
-    }
-}
 
 
 $(document).ready(function(){
-    var menu = $(".ban-actions-menu");
+    $( ".chat_ban-actions" ).click(banActions.displayMenu);
+    $( ".ban-actions__act" ).click(banActions.sendData);
 
     $(document).scroll(function () {
-        menu.css("display", "none");
+        banActions.getMenu.css("display", "none");
     });
 
     $(".chat__dialog").scroll(function () {
-        var menu = $(".ban-actions-menu");
-        menu.css("display", "none");
+        banActions.getMenu.css("display", "none");
     });
 
+
 });
+
+
+
+
+/*
+
+// Hide the modal dialog when someone clicks outside of it.
+$(".ban-actions-menu").bind( "clickoutside", function(event){
+    if ( banActions.getMenu.css("display") == "block") {
+        console.log(banActions.getMenu.css("display"));
+        // $(this).hide();
+    }
+});
+
+$(document.body).click(function(e){
+    var $box = banActions.getMenu;
+    var isBlock = false;
+    console.log(e.target);
+
+    if(e.target.class !== '.ban-actions-menu' && !$.contains($box[0], e.target) && isBlock) {
+        $box.remove();
+        isBlock = true;
+    }
+
+});
+
+ $(document).click(function(e) {
+ var target = e.target;
+
+ if (!$(target).is('.ban-actions-menu') && !$(target).parents().is('.ban-actions-menu')) {
+ $('.ban-actions-menu').hide();
+ }
+ });
+
+
+
+*/
 
